9447fc6530a6bab9cbb5b0701f513f2a949f7c51
Dmitry Kuzmenko <dmytrokzmnk@gmail.com>
2021-04-08 18:29:28 +0300

    action setUser and setToken(dev)

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,29 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import Login from "./Login";
 import { getTokenFromResponse } from "../spotify";
 import SpotifyWebApi from "spotify-web-api-js";
 import Player from "./Player";
 import { useStateValue } from "../Context/StateProvider";
+import { SET_USER, SET_TOKEN } from "../Context/action";
 const spotify = new SpotifyWebApi();
 
 function App() {
-  const [token, setToken] = useState(null);
-
+  const [{ user, token }, dispatch] = useStateValue();
   useEffect(() => {
     const hash = getTokenFromResponse();
     window.location.hash = "";
     let _token = hash.access_token;
 
     if (_token) {
-      setToken(_token);
-
+      dispatch({
+        type: SET_TOKEN,
+        token: _token,
+      });
       spotify.setAccessToken(_token);
 
       spotify.getMe().then((user) => {
-        console.log(user);
+        dispatch({
+          type: SET_USER,
+          user,
+        });
       });
     }
-  }, []);
+  }, [dispatch]);
 
   return (
     <div>
