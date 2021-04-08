6ba483d64519c196006a7817c7eeb78e9bf6da59
Dmitry Kuzmenko <dmytrokzmnk@gmail.com>
2021-04-08 17:13:55 +0300

    added settings login spotify

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
+import { getTokenFromResponse } from "../spotify";
 
 function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    // Set token
+    const hash = getTokenFromResponse();
+    window.location.hash = "";
+    let _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <Login />
+    <div className="app">
+      {!token && <Login />}
+      {token && <h1>im logged</h1>}
     </div>
   );
 }
